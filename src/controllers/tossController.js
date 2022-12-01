1589f741cc6336d3d487feb5483593cf7ba15758
IgnacioSEO <112772324+IgnacioSEO@users.noreply.github.com>
2022-12-01 16:00:46 +0900

    6th commit

--- src/controllers/tossController.js
+++
@@ -1,8 +0,0 @@
-const tossService = require("../services/tossService");
-
-import { loadTossPayments } from "@tosspayments/payment-sdk";
-const clientKey = "test_ck_k6bJXmgo28e2DRdXN6w3LAnGKWx4";
-
-async function main() {
-  const tossPayments = await loadTossPayments(clientKey);
-}
