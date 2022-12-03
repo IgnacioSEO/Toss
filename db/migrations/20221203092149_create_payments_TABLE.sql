dc222b6069b39a9317548f79f4bc08954e6d7d88
IgnacioSEO <112772324+IgnacioSEO@users.noreply.github.com>
2022-12-03 18:29:55 +0900

    db table change

--- db/migrations/20221203092149_create_payments_TABLE.sql
+++ db/migrations/20221203092149_create_payments_TABLE.sql
@@ -2,15 +2,16 @@
 CREATE TABLE payments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   user_id INT NOT NULL,
-  payment_key VARCHAR(200) NOT NULL,
+  payment_key VARCHAR(200) NULL,
   order_id VARCHAR(255) NOT NULL,
-  order_name VARCHAR(255) NUL;
+  order_name VARCHAR(255) NULL,
+  total_amount INT NULL,
   status VARCHAR(100) NULL,
   code VARCHAR(100) NULL,
   pg_message VARCHAR(100) NULL,
   method VARCHAR(100) NULL,
-  pg_response_status_code VARCHAR(100) NOT NULL,
-  pg_response TEXT NOT NULL,
+  pg_response_status_code VARCHAR(100) NULL,
+  pg_response TEXT NULL,
   approved_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   requested_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
 );
